c05e027a98b5b911f2c3a8b255ef337992213431
Ariel <king_arley2@yahoo.com>
2017-12-14 00:02:37 +0200

    added safe eyes for regular breaks

--- install.sh
+++ install.sh
@@ -17,6 +17,11 @@ install/install.py
 # useful programs
 sudo apt-get install tmux
 
+# avoid eye strain by taking regular breaks
+sudo add-apt-repository ppa:slgobinath/safeeyes
+sudo apt update
+sudo apt install safeeyes
+
 # build vim
 install/build-vim.sh
 
