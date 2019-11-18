ea60e2cde7dc52ab011c95d06dddfefd30660abe
Ariel Miculas <ariel.miculas@gmail.com>
2019-11-18 21:28:02 +0200

    Use clangd instead of libclang for C-family language completion in YCM

--- install/install_ycm.sh
+++ install/install_ycm.sh
@@ -2,5 +2,5 @@ curr_dir=$(pwd)
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev python3-dev
 cd ../vim.symlink/bundle/YouCompleteMe
-./install.py --clang-completer
+python3 ./install.py --clangd-completer
 cd $curr_dir
