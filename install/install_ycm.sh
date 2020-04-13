94da022abe4127e46a5859bb56e1dc54d60b0689
Ariel Miculas <ariel.miculas@gmail.com>
2020-04-13 12:44:54 +0300

    Install both clang-completer and clangd-completer

--- install/install_ycm.sh
+++ install/install_ycm.sh
@@ -2,5 +2,5 @@ curr_dir=$(pwd)
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev python3-dev
 cd ../vim.symlink/bundle/YouCompleteMe
-python3 ./install.py --clangd-completer
+python3 ./install.py --clang-completer --clangd-completer
 cd $curr_dir
