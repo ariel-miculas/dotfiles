e088d65d13455e479bb5a7b64a192354a5398471
Ariel Miculas <ariel.miculas@gmail.com>
2020-11-06 19:03:52 +0200

    Update install_ycm script

--- install/install_ycm.sh
+++ install/install_ycm.sh
@@ -1,6 +1,6 @@
 curr_dir=$(pwd)
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev python3-dev
-cd ../vim.symlink/bundle/YouCompleteMe
-python3 ./install.py --clang-completer --clangd-completer
+cd ~/.vim/bundle/YouCompleteMe
+python3 ./install.py --clangd-completer
 cd $curr_dir
