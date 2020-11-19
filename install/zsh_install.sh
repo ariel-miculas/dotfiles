53593f831fdf3756355153742ae9f76a43a10f61
Ariel Miculas <ariel.miculas@gmail.com>
2020-11-19 17:20:01 +0200

    Replace zsh theme with powerlevel10k

--- install/zsh_install.sh
+++ install/zsh_install.sh
@@ -6,3 +6,6 @@ chsh -s $(which zsh)
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 # Install syntax highlighting for currently typed commands
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
