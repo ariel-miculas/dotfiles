d0b06325598cd9ca828348c3306908f489942e61
Ariel Miculas <ariel.miculas@gmail.com>
2020-11-19 14:41:56 +0200

    Set the default shell to zsh in the zsh_install.sh script

--- install/zsh_install.sh
+++ install/zsh_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt install zsh
+# Make zsh the default shell
+chsh -s $(which zsh)
+# Install oh-my-zsh
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+# Install syntax highlighting for currently typed commands
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
