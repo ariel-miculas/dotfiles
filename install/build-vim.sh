106040087447d37e66b5709e7e61378f3294d00c
Ariel Miculas <ariel.miculas@gmail.com>
2020-11-20 23:57:49 +0200

    Add required libraries for vim clipboard support

--- install/build-vim.sh
+++ install/build-vim.sh
@@ -5,8 +5,8 @@ set -e
 curr_dir=$(pwd)
 
 sudo apt install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
-    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev \
+    libx11-dev libxpm-dev libxt-dev libxtst-dev libsm-dev \
     python3-dev libperl-dev git
 
 mkdir -p ~/work
