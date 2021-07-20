c73e784f1fd08c61925dd4206de4975cf88313d9
Ariel Miculas <ariel.miculas@gmail.com>
2021-07-20 18:19:46 +0300

    Call build_vim script from install directory to avoid duplicate code

--- install/debian/build-vim.sh
+++ install/debian/build-vim.sh
@@ -2,36 +2,17 @@
 # Building vim
 # get dependencies
 set -e
-curr_dir=$(pwd)
 
 sudo apt install -y \
     libncurses5-dev libgtk2.0-dev libatk1.0-dev libatk-adaptor \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev \
     libxtst-dev libsm-dev python3-dev libperl-dev git
 
-mkdir -p ~/work
-cd ~/work
-# clone the vim repo
-(cd vim && git fetch) || (git clone https://github.com/vim/vim.git)
-cd vim
-git checkout v8.2.1971
-make distclean
-./configure --with-features=huge \
-            --enable-multibyte \
-            --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.5/config \
-            --enable-perlinterp=yes \
-            --enable-gui=gtk2 \
-            --enable-gtk2-check \
-            --with-x \
-            --enable-cscope \
-            --prefix=/usr/local
-make VIMRUNTIMEDIR=/usr/local/share/vim/vim82
-sudo make install
+# call the generic script for building vim
+../build-vim.sh
+
 # make vim the default editor and map vi to vim
 sudo update-alternatives --install  /usr/bin/editor editor /usr/local/bin/vim 1
 sudo update-alternatives --set      editor /usr/local/bin/vim
 sudo update-alternatives --install  /usr/bin/vi vi /usr/local/bin/vim 1
 sudo update-alternatives --set      vi /usr/local/bin/vim
-
-cd $curr_dir
