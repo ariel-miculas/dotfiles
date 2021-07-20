24171bf18842e51418b67f1bc7db22becd684b82
Ariel Miculas <ariel.miculas@gmail.com>
2021-07-20 15:04:49 +0300

    Add autojump support in ranger

--- install/zsh_install.sh
+++ install/zsh_install.sh
@@ -10,6 +10,9 @@ wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - 
 # Install syntax highlighting for currently typed commands
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# Install ranger zsh r mapping
+git clone https://github.com/fdw/ranger-autojump.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ranger-autojump
+
 # Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
