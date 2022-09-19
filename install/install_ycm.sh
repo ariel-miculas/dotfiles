3938535afb5792629af15a54947d545827b76891
Ariel Miculas <ariel.miculas@gmail.com>
2022-09-19 10:57:38 +0300

    Add go and rust support to ycm

--- install/install_ycm.sh
+++ install/install_ycm.sh
@@ -2,5 +2,5 @@
 set -e
 curr_dir=$(pwd)
 cd ~/.vim/bundle/YouCompleteMe || exit
-python3 ./install.py --clangd-completer
+python3 ./install.py --clangd-completer --go-completer --rust-completer
 cd "$curr_dir"
