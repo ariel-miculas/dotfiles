d55e77f4f78b8f67732db6ca5137a248cb224357
Ariel Miculas <ariel.miculas@gmail.com>
2024-07-09 17:22:47 +0300

    home.nix: Add bat to the list of packages

--- nix/home.nix
+++ nix/home.nix
@@ -28,6 +28,7 @@
     pkgs.jq
     pkgs.vim-full
     pkgs.python3
+    pkgs.bat
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
