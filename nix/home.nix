2f8043a58683d1a29ac6af164bcd2d80d81d0a00
Ariel Miculas <ariel.miculas@gmail.com>
2024-07-09 17:06:40 +0300

    home.nix: Add tmux, fd, ripgrep, jq, vim and python3

--- nix/home.nix
+++ nix/home.nix
@@ -20,7 +20,14 @@
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs.hello
+
+    pkgs.tmux
+    pkgs.fd
+    pkgs.ripgrep
+    pkgs.jq
+    pkgs.vim-full
+    pkgs.python3
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
